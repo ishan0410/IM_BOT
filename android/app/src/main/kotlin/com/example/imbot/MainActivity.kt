d2fe6a920d02d632e9f4f61dfcd69259d62a4ee5
Ishan Madhani <ishan@usc-secure-wireless-new38.usc.edu>
2024-09-10 18:11:50 -0700

    initial commit

---
+++ android/app/src/main/kotlin/com/example/imbot/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.imbot
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants/api_consts.dart
@@ -0,0 +1,2 @@
+String BASE_URL = "https://api.openai.com/v1";
+String API_KEY = "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";
